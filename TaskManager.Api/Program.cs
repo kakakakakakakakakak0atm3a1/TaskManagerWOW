62ec10c42c8bc2e1eb40294c1eaf56fe067698ca
0atm3a1 <tima.myasnikov.95@gmail.com>
2026-08-25 22:33:42 +0300

    Fix solution file and DBContext registretion

--- TaskManager.Api/Program.cs
+++ TaskManager.Api/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
