0748451d108553823ae0a622d571c33e1bc88cf1
0atm3a1 <tima.myasnikov.95@gmail.com>
2026-09-02 14:27:02 +0300

    Add global exception handling middleware and Serilog logging

--- TaskManager.Api/Program.cs
+++ TaskManager.Api/Program.cs
@@ -3,9 +3,17 @@ using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using TaskManager.Infrastructure.Data;
 using TaskManager.Api.Validators;
+using TaskManager.Api.Middleware;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog();
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -18,6 +26,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
